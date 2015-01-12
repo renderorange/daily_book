a99c3553a29b07d55c8bddba2aa5ff37da6d1b08
Blaine Motsinger <blaine@renderorange.com>
2015-01-11 19:44:28 -0600

    tightened it up

--- scrapper.pl
+++ scrapper.pl
@@ -4,41 +4,34 @@
 
 use strict;
 use warnings;
-use Getopt::Long;
-use Switch;
 use LWP::Simple;
 
 my $VERSION = '0.0.1';
 
+use Data::Dumper;  # testing
 
-### check for and run prelim control settings
-# get opts
-my ($control);
-GetOptions (
-    "control=s" => \$control,
-);
-usage() && exit 1 unless ($control);
 
-# control
-switch ($control) {
-    case 'start'  { start() }
-    case 'stop'   { stop() }
+# get book from project gutenberg
+
+# open it up
+open (my $book_fh, "<", "pg19445.txt")
+    or die "cannot open book.txt: $!";
+
+# extract some meta things
+my ($title, $author);
+foreach (<$book_fh>) {
+    if (/Title/) {
+        chomp($title = $_);
+        $title =~ s/Title: //;
+        print "$title\n";
+    }
+    if (/Author/) {
+        chomp($author = $_);
+        $author =~ s/Author: //;
+        print "$author\n";
+    }
 }
+print "$title by $author\n";
 
-
-### subs
-sub usage {
-    print "usage: scrapper.pl [-c start|stop]\n" .
-          "\n" .
-          "options:\n" .
-          "-c | --control [start|stop]\n" .
-          "\n";
-}
-
-sub start {
-    print "start\n";
-}
-
-sub stop {
-    print "stop\n";
-}
+# close the book
+close ($book_fh);
