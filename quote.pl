80925fdf5d9505c81679d6008eccab6295e62014
Blaine Motsinger <blaine@renderorange.com>
2015-01-14 14:18:14 -0600

    added download and uncompression of catalog file

--- quote.pl
+++ quote.pl
@@ -5,10 +5,28 @@
 use strict;
 use warnings;
 
+use LWP::Simple;
+use IO::Uncompress::Bunzip2 qw(bunzip2 $Bunzip2Error);
+
 my $VERSION = '0.0.2';
 
 use Data::Dumper;  # testing
 my $testing = 0;   # testing, 1 for verbose output
+
+
+### gather pre-processing information
+# download and store the book index
+my $rc = getstore('http://www.gutenberg.org/feeds/catalog.rdf.bz2', 'catalog.rdf.bz2');
+if (is_error($rc)) {
+    die "there was an error downloading the book catalog: $rc";
+}
+
+# unpack the catalog file
+bunzip2 'catalog.rdf.bz2' => 'catalog.rdf'
+    or die "bunzip2 failed: $Bunzip2Error\n";
+
+# delete the archived version
+unlink('catalog.rdf.bz2') or warn "unable to delete catalog archive: $!";
 
 
 ### open the book, cleanup, and store
